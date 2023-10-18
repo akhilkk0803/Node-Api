fd93e66bcffbe5ccfd9af013e93faf88538408cf
akhilkk0803 <akhilkk0803@gmail.com>
2023-10-18 13:53:19 +0530

    added

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const mongoose = require("mongoose");
 const feedRoutes = require("./router/feed");
 const authRoutes = require("./router/auth");
 const userRoutes = require("./router/user");
+const compression=require('compression')
 const fileStorage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "images");
@@ -46,6 +47,7 @@ app.use((req, res, next) => {
 });
 app.options("*", cors());
 app.use(cors());
+app.use(compression())
 app.use("/images", express.static(path.join(__dirname, "images")));
 app.use("/feed", feedRoutes);
 app.use("/auth", authRoutes);
